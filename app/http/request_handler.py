c297aa3f439db9997520f55c9b4328a675a53b63
Marcin Fortuniak <fortgeek@gmail.com>
2022-06-11 01:08:07 +0200

    listing queues

--- app/http/request_handler.py
+++ app/http/request_handler.py
@@ -6,8 +6,42 @@ from requests.auth import HTTPBasicAuth
 from app.struct.connection import LeagueConnection, MobileConnection
 
 
-class RequestHandler(BaseHTTPRequestHandler):
+class InternalResponse:
+    def __init__(self, body: Optional[dict], status_code: int = 200):
+        self.body = body
+        self.status_code = status_code
+
+
+class AbstractRequestHandler(BaseHTTPRequestHandler):
     SUPPORTED_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+    def _get_body(self) -> Optional[dict]:
+        if 'Content-Length' not in self.headers:
+            return None
+
+        content_length = int(self.headers['Content-Length'])
+
+        return loads(self.rfile.read(content_length).decode())
+
+    def _respond(self, response: InternalResponse) -> None:
+        self.send_response(response.status_code)
+        self.end_headers()
+        self.wfile.write(dumps(response.body).encode())
+
+    def log_message(self, format, *args) -> None:
+        return
+
+    def _league_request(self, method: str, path: str, data: Optional[dict] = None) -> requests.Response:
+        return requests.request(
+            method,
+            f'https://127.0.0.1:{RequestHandler.CONNECTION.port}{path}',
+            json=data,
+            auth=HTTPBasicAuth('riot', RequestHandler.CONNECTION.password),
+            verify='./riotgames.pem'
+        )
+
+
+class RequestHandler(AbstractRequestHandler):
     CONNECTION: Optional[LeagueConnection] = None
 
     def __init__(self, *args, **kwargs):
@@ -18,53 +52,22 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def do(self) -> None:
         if not self.CONNECTION.open:
-            self._response(502, {'error': 'LCU connection is not yet open.'})
+            self._respond(InternalResponse({'error': 'LCU connection is not yet open.'}, 502))
             return
 
-        response = self._pass_request()
+        self._respond(self._pass_request())
 
-        self._response(response.status_code, response.json())
-
-    def _pass_request(self) -> requests.Response:
+    def _pass_request(self) -> InternalResponse:
         data = self._get_body()
-        response = requests.request(
-            self.command,
-            f'https://127.0.0.1:{self.CONNECTION.port}{self.path}',
-            json=data,
-            auth=HTTPBasicAuth('riot', self.CONNECTION.password),
-            verify='./riotgames.pem'
-        )
+        response = self._league_request(self.command, self.path, data)
 
         if response.status_code > 299:
             print(response.json())
 
-        return response
-
-    def _get_body(self) -> Optional[dict]:
-        if 'Content-Length' not in self.headers:
-            return None
-
-        content_length = int(self.headers['Content-Length'])
-        
-        return loads(self.rfile.read(content_length).decode())
-
-    def _response(self, status_code: int, body: dict):
-        self.send_response(status_code)
-        self.end_headers()
-        self.wfile.write(dumps(body).encode())
-
-    def log_message(self, format, *args):
-        return
+        return InternalResponse(response.json(), response.status_code)
 
 
-class InternalResponse:
-    def __init__(self, body: Optional[dict], status_code: int = 200):
-        self.body = body
-        self.status_code = status_code
-
-
-class InternalRequestHandler(BaseHTTPRequestHandler):
-    SUPPORTED_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+class InternalRequestHandler(AbstractRequestHandler):
     LEAGUE_CONNECTION: Optional[LeagueConnection] = None
     MOBILE_CONNECTION: Optional[MobileConnection] = None
 
@@ -75,9 +78,7 @@ class InternalRequestHandler(BaseHTTPRequestHandler):
         super().__init__(*args, **kwargs)
 
     def do(self) -> None:
-        response = self._handle_request()
-
-        self._respond(response)
+        self._respond(self._handle_request())
 
     def _handle_request(self) -> InternalResponse:
         data = self._get_body()
@@ -89,20 +90,10 @@ class InternalRequestHandler(BaseHTTPRequestHandler):
 
             return InternalResponse(None, 204)
 
+        if self.command == 'GET' and self.path == '/active-queues':
+            queues_response = self._league_request('get', '/lol-game-queues/v1/queues')
+            queues = [queue for queue in queues_response.json() if queue['queueAvailability'] == 'Available']
+
+            return InternalResponse({'data': queues}, 200)
+
         return InternalResponse({'error': 'Method and route combination not found.'}, 404)
-
-    def _get_body(self) -> Optional[dict]:
-        if 'Content-Length' not in self.headers:
-            return None
-
-        content_length = int(self.headers['Content-Length'])
-
-        return loads(self.rfile.read(content_length).decode())
-
-    def _respond(self, response: InternalResponse):
-        self.send_response(response.status_code)
-        self.end_headers()
-        self.wfile.write(dumps(response.body).encode())
-
-    def log_message(self, format, *args):
-        return
